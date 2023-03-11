a2598e8690ce8dfbd05a36c1f519e4b18888c129
gdederichs <gregor.dederichs@epfl.ch>
2023-03-11 12:54:54 +0100

    initial commit

---
+++ biosys_project.m
@@ -0,0 +1,98 @@
+clc
+clear
+
+%% Lactose input at lext = 0; arbitrary initial conditions
+% Initial cdts and params
+x0 = init_cond();
+par = param();
+% Timespan for simulation
+tspan = [0 50]; 
+options = [];
+
+[t1,x] = ode45(@diff_eq,tspan,x0,options,par);
+l = x(:, 1);
+LacI = x(:, 2);
+LacY = x(:, 3);
+LacAZ = x(:, 4);
+
+% figure(1)
+% plot(t1, l, t1, LacI, t1, LacY, t1, LacAZ)
+% legend('Intracellular Lactose', 'LacI', 'LacY', 'LacA and LacZ')
+% xlabel('time')
+% ylabel('concentration')
+
+%% Lactose input at lext = 1; initial cdts are steady state of previous
+x0 = [l(end), LacI(end), LacY(end), LacAZ(end)]; %x0 = [lin; LacI0; LacY0; LacAZ0];
+par.lext = 1;
+
+[t2,x] = ode45(@diff_eq,tspan,x0,options,par);
+l_1 = x(:, 1);
+LacI_1 = x(:, 2);
+LacY_1 = x(:, 3);
+LacAZ_1 = x(:, 4);
+
+% figure(2)
+% plot(t2, l_1, t2, LacI_1, t2, LacY_1, t2, LacAZ_1)
+% legend('Intracellular Lactose', 'LacI', 'LacY', 'LacA and LacZ')
+% xlabel('time')
+% ylabel('concentration')
+
+%% Merge graphs
+
+l_merge = cat(1,l,l_1);
+LacI_merge = cat(1,LacI,LacI_1);
+LacY_merge = cat(1,LacY,LacY_1);
+LacAZ_merge = cat(1,LacAZ,LacAZ_1);
+t_merge = cat(1,t1,t1(end)+t2);
+
+figure(3)
+hold on
+grid minor
+plot(t_merge, l_merge, t_merge, LacI_merge, t_merge, LacY_merge, t_merge, LacAZ_merge)
+xline(t1(end),Label = "Extracellular Lactose added",LineStyle="--")
+legend('Intracellular Lactose', 'LacI', 'LacY', 'LacA and LacZ','',Location = 'northwest')
+xlabel('time')
+ylabel('concentration')
+hold off
+%% Functions
+function dxdt = diff_eq(t,x,par)
+% Variables 
+l = x(1);
+LacI = x(2);
+LacY = x(3);
+LacAZ = x(4);
+
+% Differential equations
+l_dot = par.k1*par.lext*LacY - l*par.kd*LacAZ;
+LacI_dot = par.k2/(1+(l/par.Kl)^par.n)-par.kdI*LacI;
+LacY_dot = par.k3/(1+(LacI/par.KI)^par.m)-par.kdY*LacY;
+LacAZ_dot = 2*par.k3/(1+(LacI/par.KI)^par.m)-par.kdAZ*LacAZ;
+
+dxdt = [l_dot;LacI_dot;LacY_dot;LacAZ_dot];
+end
+
+
+function par = param()
+par.k1 = 0.9; %absorption rate of lext to l
+par.k2 = 1; %formation of LacI
+par.k3 = 3; %formation of LacY, LacA and LacZ
+par.kd = 0.5; %degradation of l
+par.kdI = 0.4; %degradation of LacI
+par.kdY = 0.2; %degradation of LacY
+par.kdAZ = 0.2; %degradation of LacA and LacZ
+par.Kl = 0.03; %inhibition of LacI (smaller value -> bigger inhinition)
+par.KI = 0.05; %inhibition of LacY, LacA and LacZ (smaller value -> bigger inhinition)
+par.lext = 0.0; %lext constant
+par.n = 1;
+par.m = 1;
+end
+
+
+function x0 = init_cond()
+lin = 6;
+LacI0 = 0.01;
+LacY0 = 1;
+LacAZ0 = 2*LacY0;
+
+x0 = [lin; LacI0; LacY0; LacAZ0];
+end
---
+++ biosys_project_minimal_model.m
@@ -0,0 +1,109 @@
+clc
+clear
+
+%% Minimal Model Simulation
+% Initial cdts and params
+x0 = init_cond();
+par = param();
+% Timespan for simulation
+tspan = [0 20]; 
+options = [];
+
+[t1,x] = ode45(@diff_eq,tspan,x0,options,par);
+l = x(:, 1);
+LacY = x(:, 2);
+
+figure(1)
+hold on
+grid minor
+plot(t1, l, t1, LacY)
+legend('Intracellular Lactose', 'LacY')
+xlabel('time')
+ylabel('concentration')
+
+%% Jacobian, augmentation
+% syms l LacY beta lext gamma delta sigma p l0
+% 
+% l_dot = beta*lext*LacY-gamma*l;
+% LacY_dot = delta+p*l^4/(l^4+l0^4)-sigma*LacY;
+% 
+% A = jacobian([l_dot,LacY_dot],[l LacY]);
+% B = jacobian([l_dot,LacY_dot],[beta lext gamma delta sigma p l0]);
+% 
+% % S_dot = A*S + B
+
+%% Nullclines
+plot_NullCline(6,12)
+vectorField(par,6, 12)
+title('Nullclines')
+xlabel('[LacY]')
+ylabel('[Lactose]')
+legend('dLacY/dt = 0','dl/dt = 0')
+view([90 -90])
+
+
+
+%% Functions
+function nl = plot_NullCline(x_lim, y_lim)
+par = param();
+
+syms l LacY 
+LacY_dot = par.delta+par.p*l^4/(l^4+par.l0^4)-par.sigma*LacY;
+l_dot = par.beta*par.lext*LacY-par.gamma*l;
+
+
+figure()
+hold on
+grid minor
+nl1=ezplot(LacY_dot,[0,x_lim,0,y_lim]);
+set(nl1,color = 'r');
+nl2=ezplot(l_dot,[0,x_lim,0,y_lim]);
+set(nl2,color = 'b');
+nl=[nl1,nl2];
+end
+
+function vectorField(par, x_lim, y_lim)
+[LacY ,l] = meshgrid(0:0.5:x_lim, 0:0.5:y_lim);
+
+LacY_dot = par.delta+par.p.*l.^4./(l.^4+par.l0.^4)-par.sigma.*LacY;
+l_dot = par.beta.*par.lext.*LacY-par.gamma.*l;
+
+quiver(LacY, l, LacY_dot, l_dot)
+xlim([0 x_lim])
+ylim([0 y_lim])
+
+end
+
+
+
+function dxdt = diff_eq(t,x,par)
+% Variables 
+l = x(1);
+LacY = x(2);
+
+% Differential equations
+l_dot = par.beta*par.lext*LacY-par.gamma*l;
+LacY_dot = par.delta+par.p*l^4/(l^4+par.l0^4)-par.sigma*LacY;
+
+
+dxdt = [l_dot;LacY_dot];
+end
+
+
+function par = param()
+par.beta = 1;
+par.gamma = 1;
+par.delta = 0.2;
+par.sigma = 1;
+par.l0 = 4;
+par.p = 4;
+par.lext = 2.5;
+end
+
+
+function x0 = init_cond()
+l_init = 4;
+LacY0 = 1;
+
+x0 = [l_init; LacY0];
+end

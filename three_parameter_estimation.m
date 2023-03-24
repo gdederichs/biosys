3477cbdd69c4529d904ce4976f1b88a1de7517e2
gdederichs <gregor.dederichs@epfl.ch>
2023-03-24 12:18:49 +0100

    Results for three params

--- three_parameter_estimation.m
+++ three_parameter_estimation.m
@@ -16,69 +16,68 @@ lsqnonlin(@residual,  log10 (initParams), log10 (loBound), log10 (upBound), opts
 
 function R = residual(b)
         
-   a = 10.^b;
-         
-   [T,Y]= reactionsolve(a);
-       
-   residual = exp - Y;
-   R = residual(:);
-       
-   results=a;
-       
-   subplot(2,1,1);
-   plot(T,Y(:,1));
-   hold on;
-   plot(tspan(:,1),exp(:,1),'o');
-   xlabel('Time');
-   ylabel('Act');
-   hold off;
-   str = sprintf('%g | ', a);
-   title(str);
+a = 10.^b;
+[T,Y]= reactionsolve(a);
+residual = exp - Y;
+R = residual(:);
+results=a;
+   
+subplot(2,1,1);
+plot(T,Y(:,1));
+hold on;
+grid minor
+plot(tspan(:,1),exp(:,1),'*');
+xlabel('Time');
+ylabel('Act');
+hold off;
+str = "Parameters: " + sprintf('%g | ', a);
+title(str); 
 
-         
-   subplot(2,1,2);
-   plot(T,Y(:,2));
-   hold on;
-   plot(tspan(:,1),exp(:,2),'o');
-   xlabel('Time');
-   ylabel('yP');
-   hold off;
-          
-   hold off;
-   drawnow;
+     
+subplot(2,1,2);
+plot(T,Y(:,2));
+hold on;
+grid minor
+plot(tspan(:,1),exp(:,2),'*');
+xlabel('Time');
+ylabel('yP');
+hold off;
+      
+hold off;
+drawnow;
 end
 
 function [T,Y] = reactionsolve(a)
 
-    s=a(1);
-    km4=a(2);
-    km5=a(3);
+s=a(1);
+km4=a(2);
+km5=a(3);
 
-    % exp(tspan = 0)
-    x0 = [0.8; 0.6];
+% exp(tspan = 0)
+x0 = [0.8; 0.6];
 
-    [T,Y] = ode45(@reaction, tspan, x0, []);
+[T,Y] = ode45(@reaction, tspan, x0, []);
 
 function dx = reaction(t,x)
 
-    Act=x(1);
-    yp=x(2);
+Act=x(1);
+yp=x(2);
 
-    par.ytot=1;
-    par.E=0.2;
+par.ytot=1;
+par.E=0.2;
 
-    par.k1 = 0.1;
-    par.k2 = 1.1324;
-    par.k3 = 1.5;
-    par.k4 = 0.1;
-    par.k5 = 0.502;
+par.k1 = 0.1;
+par.k2 = 1.1324;
+par.k3 = 1.5;
+par.k4 = 0.1;
+par.k5 = 0.502;
 
-    Act_dot = par.k1*s+par.k2*yp-par.k3*Act;
-    yp_dot = par.k4*Act*(par.ytot-yp)/(km4)-par.k5*par.E*yp/(km5+yp);
+Act_dot = par.k1*s+par.k2*yp-par.k3*Act;
+yp_dot = par.k4*Act*(par.ytot-yp)/(km4)-par.k5*par.E*yp/(km5+yp);
 
-    dx = zeros(2,1);
-    dx(1)=Act_dot;
-    dx(2)=yp_dot;
+dx = zeros(2,1);
+dx(1)=Act_dot;
+dx(2)=yp_dot;
     
 end
 

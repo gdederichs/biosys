bdb4a7c746f4044c68da288bda2fa2869a6dafac
LeFrom <romain.lefebvre@epfl.ch>
2023-03-25 15:04:56 +0100

    Hopefully last commit

--- noise_parameter_estimation.m
+++ noise_parameter_estimation.m
@@ -6,9 +6,9 @@ tspan = data.tspan;
 exp = data.exp;
 
 % Adding noise (var = 0.01, sd = 0.1)
-for i = 1:length(tspan)
-    tspan(i) = tspan(i)+randn(1,1)*0.04; 
-    exp(i) = exp(i)+randn(1,1)*0.04;
+for j = 1:length(tspan)
+    exp(j, 1) = exp(j, 1)+randn(1,1)*0.2;
+    exp(j, 2) = exp(j, 2)+randn(1,1)*0.2;
 end
 
 opts = optimset('TolFun', 1e-12, 'TolX', 1e-12, 'MaxIter', 150, 'Diagnostics', 'off', 'Display', 'iter');
@@ -22,68 +22,77 @@ lsqnonlin(@residual,  log10 (initParams), log10 (loBound), log10 (upBound), opts
 
 function R = residual(b)
         
-   a = 10.^b;
+a = 10.^b;
          
-   [T,Y]= reactionsolve(a);
+[T,Y]= reactionsolve(a);
+residual = zeros([length(tspan), 2]);
+for i = 1:length(T)
+    for ii = 1:length(tspan)
+        if T(i) == tspan(ii)
+            residual(ii, 1) = exp(ii, 1)-Y(i, 1);
+            residual(ii, 2) = exp(ii, 2)-Y(i, 2);
+        end
+    end
+end
+R = residual(:);
+SSE = sum(R(:).^2);
+results=a;
        
-   residual = exp - Y;
-   R = residual(:);
-       
-   results=a;
-       
-   subplot(2,1,1);
-   plot(T,Y(:,1));
-   hold on;
-   plot(tspan(:,1),exp(:,1),'o');
-   xlabel('Time');
-   ylabel('Act');
-   hold off;
-   str = sprintf('%g | ', a);
-   title(str);
+subplot(2,1,1);
+plot(T,Y(:,1));
+hold on;
+grid minor
+plot(tspan(:,1),exp(:,1),'*');
+xlabel('Time');
+ylabel('Act');
+hold off;
+str = "Parameters: " + sprintf('%g | ', a);
+title(str);
 
          
-   subplot(2,1,2);
-   plot(T,Y(:,2));
-   hold on;
-   plot(tspan(:,1),exp(:,2),'o');
-   xlabel('Time');
-   ylabel('yP');
-   hold off;
+subplot(2,1,2);
+plot(T,Y(:,2));
+hold on;
+grid minor
+plot(tspan(:,1),exp(:,2),'*');
+xlabel('Time');
+ylabel('yP');
+hold off;
           
-   hold off;
-   drawnow;
+hold off;
+drawnow;
 end
 
 function [T,Y] = reactionsolve(a)
 
-    s=a(1);
-    k1=a(2);
-    k2=a(3);
-    k3=a(4);
-    k4=a(5);
-    k5=a(6);
-    km4=a(7);
-    km5=a(8);
+s=a(1);
+k1=a(2);
+k2=a(3);
+k3=a(4);
+k4=a(5);
+k5=a(6);
+km4=a(7);
+km5=a(8);
 
-    % exp(tspan = 0)
-    x0 = [exp(1,1); exp(1,2)];
-
-    [T,Y] = ode45(@reaction, tspan, x0, []);
+% exp(tspan = 0)
+x0 = [0.8; 0.6];
+t = 0:0.01:12;
+[T,Y] = ode45(@reaction, t, x0, []);
 
 function dx = reaction(t,x)
 
-    Act=x(1);
-    yp=x(2);
+Act=x(1);
+yp=x(2);
 
-    par.ytot=1;
-    par.E=0.2;
+par.ytot=1;
+par.E=0.2;
 
-    Act_dot = k1*s+k2*yp-k3*Act;
-    yp_dot = k4*Act*(par.ytot-yp)/(km4+par.ytot-yp)-k5*par.E*yp/(km5+yp);
+Act_dot = k1*s+k2*yp-k3*Act;
+yp_dot = k4*Act*(par.ytot-yp)/(km4+par.ytot-yp)-k5*par.E*yp/(km5+yp);
 
-    dx = zeros(2,1);
-    dx(1)=Act_dot;
-    dx(2)=yp_dot;
+dx = zeros(2,1);
+dx(1)=Act_dot;
+dx(2)=yp_dot;
     
 end
 
